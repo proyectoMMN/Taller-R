c8876ae9bd2e2b9a1c2e8d7072f5a7ecd30c3adb
proyectoMMN <120412372+proyectoMMN@users.noreply.github.com>
2025-10-24 16:01:17 -0300

    Add files via upload

---
+++ ejercicio titanic.R
@@ -0,0 +1,77 @@
+# Analisis de datos titanic ----
+
+## Cargar librerias ----
+
+
+## Importar y guardar base de datos ----
+
+
+
+## Primeras filas ----
+
+
+
+## Ultimas filas ----
+
+
+
+## Cantidad de filas ----
+
+
+
+## Mostrar los nombres de las variables ----
+
+
+
+## Filtrar los pasejeros con edad mayor a 30 ----
+
+
+
+## Filtrar los pasajeros de primera clase ----
+
+
+
+## Pasajeros que sobrevivieron y que pagaron mas de $100,000 ----
+
+
+
+## Seleccionar variables de numero de pasajero, nombre y embarque ----
+
+
+
+## Retener variables excepto ticket y cabina ----
+
+
+
+## Reordenar columnas para que nombre aparezca despues de numero de pasajeros ----
+
+
+## Seleccionar las variables que terminen con "a” ----
+
+
+## Pasajeros de menor a mayor edad ----
+
+
+## Pasajero ordenados de mayor a menor tarifa cancelada ----
+
+
+
+## Mostrar a los 10 pasajeros que mayor edad ----
+
+
+## Mostrar al 20% de los pasajeros que pagaron menos ----
+
+
+
+## Crear puntaje total de familiares a bordo para cada pasajero ----
+
+
+## Mostrar los valores únicos de embarque ----
+
+
+## Describir el porcentaje de datos perdidos en total y por variable ----
+
+
+## Retener únicamente pasajeros con datos completos ----
+
+
